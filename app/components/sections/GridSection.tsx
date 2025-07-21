cc5f9935e4d2ba75b5ca7ac3e26b14130248cfcf
Ashfinn <obidur.shawal@gmail.com>
2025-07-21 08:56:49 +0600

    modified:   app/components/sections/GridSection.tsx 	modified:   app/favicon.ico

--- app/components/sections/GridSection.tsx
+++ app/components/sections/GridSection.tsx
@@ -62,7 +62,7 @@ const itemVariants = {
   visible: {
     opacity: 1,
     y: 0,
-    transition: { type: 'spring', stiffness: 100, damping: 10 },
+    transition: { type: "spring" as const, stiffness: 100, damping: 10 },
   },
 };
 
