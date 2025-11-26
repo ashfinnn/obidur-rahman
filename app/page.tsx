7a2b23c49000d7c9d7ed988b2dd4da3a67d55021
ashfinnn <obidur.shawal@gmail.com>
2025-11-26 15:15:59 +0600

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Hero from './components/Hero';
 import AboutSection from './components/sections/AboutSection';
 import ContactSection from './components/sections/ContactSection';
 import ProjectsSection from './components/sections/ProjectsSection';
-import SkillsSection from './components/sections/SkillsSection';
 import Footer from './components/Footer';
 import SlideSection from './components/ui/SlideSection';
 
